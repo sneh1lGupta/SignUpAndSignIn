961e6f0d7159f94a71fca7615ea6cc88d969258d
snehil gupta <snehil.gupta@unthinkable.co>
2022-03-30 11:20:37 +0530

    Connected to Database

---
+++ SignupAndSignin/Migrations/20220330054907_settingUp.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SignupAndSignin.Migrations
+{
+    public partial class settingUp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Registrations",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registrations", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registrations");
+        }
+    }
+}
---
+++ SignupAndSignin/Models/Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignupAndSignin.Models
+{
+    public class Users
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public string email { get; set; }
+
+        [Required]
+        public string password { get; set; }
+    }
+}
